15060f0b8ec3bf814030ca444668b73af0851f6c
Ruslan <fallrabble@gmail.com>
2013-04-29 12:49:01 +0400

    added some changes

--- test/ResourcePool.py
+++ test/ResourcePool.py
@@ -5,15 +5,29 @@ __author__ = 'Administrator'
 
 class ResourcePool(Topology):
     def __init__(self, name):
-        self.name = name
-
-    def destroy(self, name=None, save_vms=False):
         if name is None:
-            name = self.name
+            raise Exception('Do not specify the name of the resource pool')
+        else:
+            self.name = name
+
+    def create(self):
+        try:
+            self.manager.create_resource_pool(name=self.name,
+                                              esx_hostname=self.esx_host)
+        except self.exception as error:
+            raise error
+
+    def destroy(self, save_vms=False):
+        """
+        Destroys the resource pool
+        :param save_vms: if True  - deletes all vms in this resource pool
+                         if False - save vms and move its to the up resource pool
+        :raise: CreatorException
+        """
         try:
             if save_vms is True:
-                self.manager.destroy_resource_pool(name, self.config.esx_host)
+                self.manager.destroy_resource_pool(self.name, self.config.esx_host)
             else:
-                self.manager.destroy_resource_pool_with_vms(name, self.config.esx_host)
-        except Exception as error:
+                self.manager.destroy_resource_pool_with_vms(self.name, self.config.esx_host)
+        except self.exception as error:
             raise error
