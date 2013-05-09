68a99afe06de474698fbf0698c30acda55510337
rkhozinov <rkhozinov@mirantis.com>
2013-05-09 04:38:19 +0400

    change some methods

--- tests/test_serial_connect.py
+++ tests/test_serial_connect.py
@@ -9,18 +9,24 @@ esx_host = '172.18.93.30'
 esx_password = 'swordfish'
 esx_login = 'root'
 
-# session = RemoteControl(esx_host, esx_login, esx_password)
-#
-# cmd = "nc -U /vmfs/volumes/datastore1/pipe_client/pipe_client"
-#
-#
-# print session.perform(cmd)
-# print session.perform('vyatta')
 
 client = paramiko.SSHClient()
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-client.connect(esx_host, 22, esx_login, esx_password)
-stdin, stdout, stderr = client.exec_command("nc -U /vmfs/volumes/datastore1/pipe_client/pipe_client",get_pty=True)
+client.connect(esx_host, 22, esx_login, esx_password,allow_agent=False,)
 
-print stdout.read()
-print stderr.read()
+
+stdin, stdout, stderr = client.exec_command('nc -U /vmfs/volumes/datastore1/pipe_client/pipe_client', get_pty=True)
+print stderr.readlines()
+print stdout.readlines()
+
+stdin, stdout, stderr = client.exec_command('vyatta')
+print stderr.readlines()
+print stdout.readlines()
+
+stdin, stdout, stderr = client.exec_command('vyatta')
+print stderr.readlines()
+print stdout.readlines()
+
+client.close()
+
+
