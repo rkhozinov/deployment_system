1a72fe75c9665137ac8fb4669d93dc1c56505685
rkhozinov <rkhozinov@mirantis.com>
2013-05-13 13:29:27 +0400

    changed arhictecture

--- deployment_system/VirtualMachine.py
+++ deployment_system/VirtualMachine.py
@@ -1,39 +1,41 @@
 from VMController import VMController
-from lib.Hatchery import Creator as manager
 from lib.Hatchery import CreatorException as exception
 
 
 class VirtualMachine(object):
-    def __init__(self, name, login, password, memory=512, cpu=2, size=1024, connected_networks=None, iso=None,
+    def __init__(self, name, login, password, resource_pool, manager, esx_host, memory=512, cpu=2, size=1024,
+                 connected_networks=None, iso=None,
                  description=None, neighbours=None, configuration=None):
-        """
-        ESXi virtual machine
-        :rtype : VirtualMachine
-        :param login:
-        :param password: 
-        :param configuration: 
-        :param name: virtual machine name
-        :param connected_networks:
-        :param iso:
-        :param memory:
-        :param cpu:
-        :param size:
-        :param description:
-        :param neighbours:
-        """
 
-        if not name:
-            raise ValueError('Virtual machine name is not exist')
-        else:
+        if name:
             self.name = name
-        if not login:
-            raise ValueError('Virtual machine login is not exist')
         else:
+            raise ValueError('Virtual machine name is not exist')
+
+        if login:
             self.login = login
-        if not password:
-            raise ValueError('Resource pool name is not exist')
         else:
+            raise ValueError('Virtual machine login is not exist')
+
+        if password:
             self.password = password
+        else:
+            raise ValueError('Virtual machine password is not exist')
+
+        if resource_pool:
+            self.resource_pool = resource_pool
+        else:
+            raise AttributeError('Resource pool is not exist')
+
+        if esx_host:
+            self.esx_host = esx_host
+        else:
+            raise AttributeError('ESX host address is not exist')
+
+        if manager:
+            self.manager = manager
+        else:
+            raise AttributeError('ESX manager address is not exist')
 
         self.memory = memory
         self.cpu = cpu
@@ -48,21 +50,15 @@ class VirtualMachine(object):
         self.iso = iso
         self.serial_port_path = None
 
-    def create(self, resource_pool, esx_host):
-        if not resource_pool:
-            raise ValueError('Resource pool name is not exist')
-        if not esx_host:
-            raise ValueError('Resource pool name is not exist')
-
+    def create(self):
         try:
-            pass
-            # manager.create_vm(self.name, esx_host, self.iso,
-            #                   resource_pool='/' + resource_pool,
-            #                   networks=self.connected_networks,
-            #                   annotation=self.description,
-            #                   memorysize=self.memory,
-            #                   cpucount=self.cpu,
-            #                   disksize=self.size)
+            self.manager.create_vm(self.name, self.esx_host, self.iso,
+                                   resource_pool='/' + self.resource_pool.name,
+                                   networks=self.connected_networks,
+                                   annotation=self.description,
+                                   memorysize=self.memory,
+                                   cpucount=self.cpu,
+                                   disksize=self.size)
         except exception as error:
             raise error
 
@@ -76,16 +72,19 @@ class VirtualMachine(object):
 
     def destroy(self):
         try:
-            manager.destroy_vm(vmname=self.name)
+            self.manager.destroy_vm(self.name)
         except exception as error:
             raise error
 
     def configure(self, esx_host, esx_login, esx_password):
         #TODO: add configure source
-        vm_ctrl = VMController(vm=self,
-                               esx_host=esx_host,
-                               esx_login=esx_login,
-                               esx_password=esx_password)
+        try:
+            vm_ctrl = VMController(vm=self,
+                                   esx_host=esx_host,
+                                   esx_login=esx_login,
+                                   esx_password=esx_password)
 
-        for option in self.configuration:
-            vm_ctrl.cmd(option)
+            for option in self.configuration:
+                vm_ctrl.cmd(option)
+        except Exception as error:
+            raise error
