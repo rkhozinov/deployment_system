57b4a056c7fe057a219c9c34d2f2c30201fedb21
rkhozinov <rkhozinov@mirantis.com>
2013-05-13 18:10:42 +0400

    fix tests

--- tests/unittests/TestVMController.py
+++ tests/unittests/TestVMController.py
@@ -1,5 +1,6 @@
 import unittest
 import logging
+import lib.Hatchery as vmmanager
 from deployment_system.TopologyReader import TopologyReader
 from deployment_system.VMController import VMController
 from deployment_system.VirtualMachine import VirtualMachine
@@ -8,33 +9,31 @@ from deployment_system.VirtualMachine import VirtualMachine
 class TestVMController(unittest.TestCase):
     def setUp(self):
         self.config_path = '../etc/topology.ini'
-        self.name = 'topology'
+        self.rpname = 'rkhozinov'
+        self.vmname = 'pipe_client'
+        self.vmlogin = 'vyatta'
+        self.vmpassword = 'vyatta'
         self.config = TopologyReader(self.config_path)
-        self.log = logging.getLogger(__name__)
+        self.manager = vmmanager.Creator(self.config.esx_host,
+                                         self.config.esx_login,
+                                         self.config.esx_password)
+        self.logger = logging.getLogger(__name__)
         logging.basicConfig()
 
     def test_create_instance(self):
-
         try:
             vms = self.config.get_virtual_machines()
-            for vm in vms:
-                vm_ctrl = VMController(vm=vm,
-                                       esx_host=self.config.esx_host,
-                                       esx_login=self.config.esx_login,
-                                       esx_password=self.config.esx_password)
-                self.assertIsInstance(vm_ctrl, VMController)
+            vm_ctrl = VMController(vm=vms[0],
+                                   esx_host=self.config.esx_host,
+                                   esx_login=self.config.esx_login,
+                                   esx_password=self.config.esx_password)
+            self.assertEqual(vm_ctrl.vm, vms[0])
+            self.assertIsInstance(vm_ctrl.vm, VirtualMachine)
+            self.assertIsInstance(vm_ctrl, VMController)
         except Exception as error:
-            self.assertFalse(True)
-            self.log.critical(error.message)
+            self.logger.critical(error.message)
+            self.fail(error.message)
 
-    def test_vm_configure(self):
-
-        vm = VirtualMachine()
-        vmctrl = VMController(vm=vm,
-                               esx_host=self.config.esx_host,
-                               esx_login=self.config.esx_login,
-                               esx_password=self.config.esx_password)
-        self.assertEqual()
 
 if __name__ == "__main__":
     unittest.main()
