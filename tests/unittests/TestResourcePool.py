69d7d6636781969b5ee37d9527664a398b7332ce
rkhozinov <rkhozinov@mirantis.com>
2013-05-13 13:29:13 +0400

    added some tests

--- tests/unittests/TestResourcePool.py
+++ tests/unittests/TestResourcePool.py
@@ -11,30 +11,41 @@ __author__ = 'rkhozinov'
 class TestResoursePool(unittest.TestCase):
     def setUp(self):
         self.config_path = '../etc/topology.ini'
-        self.rpname = 'topology'
+        self.rpname = 'test_pool'
         self.treader = TopologyReader(self.config_path)
         self.manager = vm_manager.Creator(self.treader.esx_host,
-                                         self.treader.esx_login,
-                                         self.treader.esx_password)
+                                          self.treader.esx_login,
+                                          self.treader.esx_password)
         self.logger = logging.getLogger(__name__)
         logging.basicConfig()
 
     def testCreateInstance(self):
         try:
-
-            rpool = ResourcePool(self.rpname)
+            rpool = ResourcePool(self.rpname, self.manager)
             self.assertIsInstance(rpool, ResourcePool)
             self.assertEqual(rpool.name, self.rpname)
-        except:
+        except AttributeError as e:
+            self.logger.critical(e.message)
             self.assertTrue(False)
 
     def testCreateResourcePool(self):
         try:
-            rpool = ResourcePool(name=self.rpname)
-            rpool.create(self.manager)
+            ResourcePool(self.rpname, self.manager).create()
         except Exception as e:
             self.logger.critical(e.message)
             self.assertTrue(False)
+
+    def test_destroy_resourse_pool(self):
+        try:
+            ResourcePool(self.rpname, self.manager).destroy()
+        except Exception as e:
+            self.logger.critical(e.message)
+
+    def test_destroy_resource_pool_with_vms(self):
+        try:
+            ResourcePool(self.rpname, self.manager).destroy(with_vms=True)
+        except Exception as e:
+            self.logger.critical(e.message)
 
 
 if __name__ == "__main__":
