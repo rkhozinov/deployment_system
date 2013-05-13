53b049a93b620b29ffccd1e1999e895bad787be9
rkhozinov <rkhozinov@mirantis.com>
2013-05-13 16:10:18 +0400

    change create() and destroy()

--- deployment_system/ResourcePool.py
+++ deployment_system/ResourcePool.py
@@ -2,44 +2,46 @@ from lib.Hatchery import CreatorException as exception
 
 
 class ResourcePool(object):
-    def __init__(self, name, manager):
+    def __init__(self, name):
         """
         ESXi resource pool instance
         :param manager: ESXi manager
         :param name: ESXi name of a resource pool
         :raise:
         """
-        if not name:
+        if name and isinstance(name, str):
             self.name = name
         else:
             raise AttributeError('Do not specify the name of the resource pool')
-        if manager:
-            self.manager = manager
-        else:
-            raise AttributeError('Do not specify the ESX manager')
 
 
-    def create(self):
+    def create(self, manager):
         """
         Creates a ESXi resource pool
-        :raise: CreationException
+        :raise: AttributeExcpetion, CreatorExcpetion
         """
+        if not manager:
+            raise AttributeError('Do not specify the ESX manager')
         try:
-            self.manager.create_resource_pool(self.name)
+
+            manager.create_resource_pool(self.name)
         except exception as error:
             raise error
 
-    def destroy(self, with_vms=False):
+    def destroy(self, manager, with_vms=False):
         """
         Destroys the resource pool
         :param with_vms: if True  - deletes all vms in this resource pool
                          if False - save vms and move its to the up resource pool
         :raise: CreatorException
         """
+        if not manager:
+            raise AttributeError('Do not specify the ESX manager')
+
         try:
             if with_vms:
-                self.manager.destroy_resource_pool_with_vms(self.name)
+                manager.destroy_resource_pool_with_vms(self.name)
             else:
-                self.manager.destroy_resource_pool(self.name)
+                manager.destroy_resource_pool(self.name)
         except exception as error:
             raise error
