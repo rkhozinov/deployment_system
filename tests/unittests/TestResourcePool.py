49bb7c758870b10b475a31d59962e8d62aaf773c
rkhozinov <rkhozinov@mirantis.com>
2013-05-13 12:12:59 +0400

    fixed some tests

--- tests/unittests/TestResourcePool.py
+++ tests/unittests/TestResourcePool.py
@@ -11,51 +11,29 @@ __author__ = 'rkhozinov'
 class TestResoursePool(unittest.TestCase):
     def setUp(self):
         self.config_path = '../etc/topology.ini'
-        self.name = 'topology'
-
-        self.log = logging.getLogger(__name__)
+        self.rpname = 'topology'
+        self.treader = TopologyReader(self.config_path)
+        self.manager = vm_manager.Creator(self.treader.esx_host,
+                                         self.treader.esx_login,
+                                         self.treader.esx_password)
+        self.logger = logging.getLogger(__name__)
         logging.basicConfig()
 
     def testCreateInstance(self):
         try:
-            treader = TopologyReader(self.config_path)
-            rpool = ResourcePool(self.name, treader.esx_host)
+
+            rpool = ResourcePool(self.rpname)
             self.assertIsInstance(rpool, ResourcePool)
-            self.assertEqual(rpool.name, self.name)
+            self.assertEqual(rpool.name, self.rpname)
         except:
             self.assertTrue(False)
 
-    def testCreateRespourcePool(self):
-        treader = TopologyReader(self.config_path)
-
-        rpool = None
+    def testCreateResourcePool(self):
         try:
-            manager = vm_manager.Creator(treader.esx_host, treader.esx_login, treader.esx_password)
-            rpool = ResourcePool(manager, self.name)
+            rpool = ResourcePool(name=self.rpname)
+            rpool.create(self.manager)
         except Exception as e:
-            self.log.critical(e.message)
-            self.assertTrue(False)
-
-        try:
-            rpool.create()
-        except Exception as e:
-            self.log.critical(e.message)
-            self.assertTrue(False)
-
-    def testCreateResourcePoolManually(self):
-        treader = TopologyReader(self.config_path)
-
-        manager = None
-        try:
-            manager = vm_manager.Creator(treader.esx_host, treader.esx_login, treader.esx_password)
-            self.assertIsInstance(manager,vm_manager.Creator)
-        except:
-            self.assertTrue(False)
-
-        try:
-            manager.create_resource_pool("topology")
-            self.assertFalse(False)
-        except:
+            self.logger.critical(e.message)
             self.assertTrue(False)
 
 
