651f9f8d078e1c28e238e954b2a779b4cd8a8d3d
rkhozinov <rkhozinov@mirantis.com>
2013-05-21 21:17:18 +0400

    fixed and refactor some test

--- tests/unittests/TestResourcePool.py
+++ tests/unittests/TestResourcePool.py
@@ -1,4 +1,5 @@
 import unittest
+from deployment_system import VirtualMachine
 from deployment_system.ResourcePool import ResourcePool
 import lib.Hatchery as Manager
 
@@ -22,31 +23,68 @@ class TestResoursePool(unittest.TestCase):
         except AttributeError as error:
             self.assertTrue(False, error.message)
 
-    def test_create_resource_pool(self):
+    def test_create_and_destroy_empty_resource_pool_on_specific_host(self):
         try:
-            ResourcePool(self.rpname).create(self.manager)
-        except Manager.ExistenceException as error:
-            self.assertTrue(True, error.message)
+            # create rp
+            try:
+                ResourcePool(self.rpname).create(self.manager, self.host_name)
+            except Manager.ExistenceException as error:
+                pass
+
+            # destroy rp
+            try:
+                ResourcePool(self.rpname).destroy(self.manager)
+            except Manager.ExistenceException as error:
+                pass
         except Manager.CreatorException as error:
             self.assertTrue(False, error.message)
 
-    def test_destroy_only_resource_pool(self):
+    def test_create_and_destroy_resource_pool_with_vms(self):
         try:
+
+            # create resource pool
+            try:
+                ResourcePool(self.rpname).create(self.manager, self.host_name)
+            except Manager.ExistenceException:
+                pass
+
+            # create vm
+            try:
+                vmname = self.rpname + 'vm'
+                password = 'vyatta'
+                user = 'vyatta'
+                vm = VirtualMachine.VirtualMachine(vmname, user, password)
+                vm.create(self.manager, self.rpname, self.host_name)
+            except Manager.ExistenceException:
+                pass
+
+            # destroy resource pool with vm
+            try:
+                ResourcePool(self.rpname).destroy(self.manager, with_vms=True)
+            except Manager.ExistenceException:
+                pass
+
+        except Manager.CreatorException as error:
+            self.assertTrue(False, error.message)
+
+    def test_try_to_create_resource_pools_with_same_name(self):
+        try:
+            # create first rp
+            try:
+                ResourcePool(self.rpname).create(self.manager, self.host_name)
+            except Manager.ExistenceException:
+                pass
+
+            # create second rp with same name
+            try:
+                ResourcePool(self.rpname).create(self.manager, self.host_name)
+            except Manager.ExistenceException:
+                self.assertTrue(True)
+
+        except Manager.CreatorException as error:
+            self.assertTrue(False, error.message)
+        finally:
             ResourcePool(self.rpname).destroy(self.manager)
-        except Manager.ExistenceException as error:
-            self.assertTrue(True, error.message)
-        except Manager.CreatorException as error:
-            self.assertTrue(False, error.message)
-
-    def test_destroy_resource_pool_with_vms(self):
-        self.test_create_resource_pool()
-        try:
-            #todo: add some virtual machines
-            ResourcePool(self.rpname).destroy(self.manager, with_vms=True)
-        except Manager.ExistenceException as error:
-            self.assertTrue(True, error.message)
-        except Manager.CreatorException as error:
-            self.assertTrue(False, error.message)
 
     def test_create_invalid_instance(self):
         try:
