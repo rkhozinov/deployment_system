17c0b8875f4febe45cd5ac43226c5c158e5c8ad9
rkhozinov <rkhozinov@mirantis.com>
2013-05-28 06:34:20 +0400

    deleted some unused files

--- tmp/asd.py
+++
@@ -1,8 +0,0 @@
-__author__ = 'rkhozinov'
-
-import logging
-
-logger = logging.getLogger(__name__)
-logging.basicConfig(format='%(asctime)s %(levelname)s:%(name)s:%(message)s',datefmt='%m/%d/%Y %H:%M:%S')
-
-logger.critical('asdasd')
