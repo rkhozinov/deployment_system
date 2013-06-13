05a2e8936d52f7de07ee6a0c16461be5243c4655
rkhozinov <rkhozinov@mirantis.com>
2013-06-13 13:34:40 +0400

    - viewed rollback mechanism - added some comments

--- deployment_system/topology.py
+++ deployment_system/topology.py
@@ -19,6 +19,7 @@ import logging
 import time
 
 from .resource_pool import ResourcePool
+from .virtual_machine import VirtualMachine
 from .switch import Switch
 from .topology_reader import TopologyReader
 from lib import hatchery as Manager
@@ -65,15 +66,19 @@ class Topology(object):
             raise e
 
     def create(self):
+        """
+        Creates all networks and virtual machines for topology
+
+        @raise: Exception, NameError
+        """
+
+        # container for rollback mechanism
         rollback = []
         try:
             # creates a resource pool for store virtual machines
             resource_pool = ResourcePool(self.resource_pool)
             resource_pool.create(self.manager)
             rollback.append(resource_pool)
-            # self.logger.info('Resource pool {} successfully created'.format(self.resource_pool))
-
-            # creates networks and switches
 
             # CREATE NETWORKS
             shared_sw_name = '%s_%s' % (self.config.SWITCH_PREFIX, self.resource_pool)
@@ -82,14 +87,14 @@ class Topology(object):
             rollback.append(shared_switch)
 
             for net in self.networks:
-                # create isolated networks
+                # creates isolated networks
                 if net.isolated:
                     sw_name = "%s_%s_%s" % (self.config.SWITCH_PREFIX, self.resource_pool, net.name)
                     switch = Switch(sw_name).create(self.manager, self.host_name)
                     rollback.append(switch)
                     switch.add_network(net, self.manager, self.host_name)
                 else:
-                    # create simple networks on shared switch
+                    # creates simple networks on shared switch
                     net.name = "%s_%s" % (self.resource_pool, net.name)
                     shared_switch.add_network(net, self.manager, self.host_name)
 
@@ -97,6 +102,7 @@ class Topology(object):
             for vm in self.vms:
                 vm.name = "{}_{}".format(self.resource_pool, vm.name)
 
+                # rename networks for virtual machine
                 for i in range(len(vm.connected_networks)):
                     for j in xrange(len(self.networks)):
                         tmp1 = self.networks[j].name.find(vm.connected_networks[i])
@@ -104,25 +110,35 @@ class Topology(object):
                             vm.connected_networks[i] = "%s_%s" % (self.resource_pool, vm.connected_networks[i])
 
                 vm.create(self.manager, self.resource_pool, self.host_name)
+
                 rollback.append(vm)
+
+                # adds serial (com) port configuration to VM for control VM via serial (com) port
                 vm.add_serial_port(manager=self.manager, host_address=self.host_address,
                                    host_user=self.host_user, host_password=self.host_password)
+
+                # add existence hard drive
                 if vm.hard_disk:
                     try:
                         vm.add_hard_disk(manager=self.manager, host_address=self.host_address,
                                          host_user=self.host_user, host_password=self.host_password,
                                          hard_disk=vm.hard_disk)
-                    except NameError as e:
-                        raise NameError()
+                    except NameError:
+                        raise
+
+                # adds VNC configuration to VM for control via VNC
                 if vm.vnc_port != '0':
                     vm.add_vnc_access(manager=self.manager, host_address=self.host_address,
                                       host_user=self.host_user, host_password=self.host_password)
+
+                # turns VM power on after configuration
                 vm.power_on(self.manager)
 
-            #todo: add boot-time
+            # wait loading virtual machine
             if len(self.vms) < 3:
                 time.sleep(120)
 
+            # configure VMs
             for vm in self.vms:
                 if 'com' in vm.config_type:
                     vm.configure_via_com(host_address=self.host_address, host_user=self.host_user,
@@ -133,31 +149,45 @@ class Topology(object):
         except Exception as e:
             self.logger.error(e.message)
             try:
+                # todo: review rollback mechanism
                 while rollback:
                     unit = rollback.pop()
+                    #if isinstance(unit,VirtualMachine):
                     if 'VirtualMachine' in str(unit.__class__):
                         unit.destroy_with_files(manager=self.manager, host_address=self.host_address,
                                                 host_user=self.host_user,
                                                 host_password=self.host_password)
+                    #elif isinstance(unit, Switch):
                     elif 'Switch' in str(unit.__class__):
                         unit.destroy(self.manager, self.config.host_name)
+                        #(Switch)(unit).destroy(self.manager, self.config.host_name)
                     elif 'ResourcePool' in str(unit.__class__):
+                    #elif isinstance(unit, ResourcePool):
                         unit.destroy(manager=self.manager)
+                        #(ResourcePool)(unit).destroy(self.manager, self.config.host_name)
             except:
                 self.logger.error("Couldn't revert changes; need to destroy manually:")
                 for unit in rollback:
+                    #if isinstance(unit,VirtualMachine):
                     if 'VirtualMachine' in str(unit.__class__):
                         self.logger.error('VM %s' % unit.name)
-
+                    #elif isinstance(unit, Switch):
                     elif 'Switch' in str(unit.__class__):
                         self.logger.error('Switch %s' % unit.name)
+                    #elif isinstance(unit, ResourcePool):
                     elif 'ResourcePool' in str(unit.__class__):
                         self.logger.error('Resource pool %s' % unit.name)
                 raise
             raise e
 
     def destroy(self):
+        """
+        Destroys topology with all networks and virtual machines
 
+        @raise: Exception
+        """
+
+        # destroys virtual machines
         for vm in self.vms:
             try:
                 vm.name = "%s_%s" % (self.resource_pool, vm.name)
@@ -165,11 +195,13 @@ class Topology(object):
                                       host_user=self.host_user,
                                       host_password=self.host_password)
             except Manager.ExistenceException:
-                self.logger.info("Couldn't find '%s' - probably it already removed" % vm.name)
+                self.logger.info("Couldn't find '%s', probably it already removed" % vm.name)
             except:
                 self.logger.error("Error with destroying VM '%s'" % vm.name)
 
         sw_name = None
+
+        # destroys isolated networks with vSwitches
         for net in self.networks:
             try:
                 if net.isolated:
@@ -181,6 +213,7 @@ class Topology(object):
             except:
                 self.logger.error("Error with destroying switch '%s'" % sw_name)
 
+        # destroys common vSwitch if exist
         try:
             shared_sw_name = '%s_%s' % (self.config.SWITCH_PREFIX, self.resource_pool)
             switch = Switch(shared_sw_name)
@@ -188,6 +221,7 @@ class Topology(object):
         except Manager.ExistenceException:
             pass
 
+        # destroys resource pool
         try:
             ResourcePool(self.resource_pool).destroy(self.manager)
         except Manager.ExistenceException:
@@ -197,6 +231,11 @@ class Topology(object):
             raise e
 
     def power_on(self):
+        """
+        Turns power on for all virtual machines in the resource pool
+
+        @raise: Exception
+        """
         for vm in self.vms:
             try:
                 vm.name = "%s_%s" % (self.resource_pool, vm.name)
@@ -206,6 +245,11 @@ class Topology(object):
                 raise
 
     def power_off(self):
+        """
+        Turns power off for all virtual machines in the resource pool
+
+        @raise: Exception
+        """
         for vm in self.vms:
             try:
                 vm.name = "%s_%s" % (self.resource_pool, vm.name)
